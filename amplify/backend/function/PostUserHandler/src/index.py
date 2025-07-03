f23f3ae3004f069d3e8ff3d9c0f95a877d9b29b6
CaptainZiboo <captainziboo@gmail.com>
2025-07-03 14:42:32 +0200

    add more detailled logs to db error

--- amplify/backend/function/PostUserHandler/src/index.py
+++ amplify/backend/function/PostUserHandler/src/index.py
@@ -123,7 +123,7 @@ def handler(event, context):
         return {
             'statusCode': 500,
             'headers': headers,
-            'body': json.dumps({'error': 'Database error'})
+            'body': json.dumps({'error': 'Database error: ' + str(e)})
         }
     except Exception as e:
         print("Unhandled exception:", e)
